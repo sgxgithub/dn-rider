577b6bf377eb1b46d1e16fd6811593f3f9298302
Sui Guoxin <gsui@voyages-sncf.com>
2017-04-10 15:59:44 +0200

    creation of the service NexusConsumer

--- grails-app/controllers/dn/rider/nexus/DnController.groovy
+++ grails-app/controllers/dn/rider/nexus/DnController.groovy
@@ -1,11 +1,16 @@
 package dn.rider.nexus
 
+import grails.converters.JSON
+
 class DnController {
+
+    def nexusConsumerService
 
     def index() { }
 
     def show() {
-        render "hello ! la fonction show !"
+        // render "hello ! la fonction show !"
+        Dn dn = nexusConsumerService.getDnConstante()
+        render dn as JSON
     }
-
 }
--- grails-app/services/dn/rider/consumer/nexus/NexusConsumerService.groovy
+++ grails-app/services/dn/rider/consumer/nexus/NexusConsumerService.groovy
@@ -1,11 +1,16 @@
 package dn.rider.consumer.nexus
 
+import dn.rider.nexus.Dn
+import grails.converters.JSON
 import grails.transaction.Transactional
 
 @Transactional
 class NexusConsumerService {
 
-    def getUrlExample() {
-        return "http://nexus/service/local/repositories/DWM-Releases/content/com/vsct/kli/delivery-notes/1.36.1/delivery-notes-1.36.1.json"
+    def getDnConstante() {
+        String url = "http://nexus/service/local/repositories/DWM-Releases/content/com/vsct/kli/delivery-notes/1.36.1/delivery-notes-1.36.1.json"
+        def dn = new Dn(JSON.parse(url))
+
+        return dn;
     }
 }
