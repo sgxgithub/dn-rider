287dd9518912b0d1958e4165d04f8a80270f97fd
Sui Guoxin <gsui@voyages-sncf.com>
2017-05-03 17:32:07 +0200

    redo home page by using bootstrap sticky footer, add individual index.css, link action to search list

--- grails-app/controllers/dn/rider/UrlMappings.groovy
+++ grails-app/controllers/dn/rider/UrlMappings.groovy
@@ -3,14 +3,15 @@ package dn.rider
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+        "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(controller:"dn")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+        //"/"(controller:"dn")
+        "/"(view: '/index')
+        "500"(view: '/error')
+        "404"(view: '/notFound')
     }
 }
